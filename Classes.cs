be634785c62b2d3b6e33fa4a70382cff254e3ee6
Scott DeHaan <scottdehaan@gmail.com>
2018-02-19 12:30:41 -0600

    Deserializer and new classes added

--- Classes.cs
+++ Classes.cs
@@ -3,20 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace package_loader
 {
-    public class Packages
+    [XmlRoot("Job")]
+    public class Job
     {
-        public List<Package> Package { get; set; }
+        [XmlElement("Package")]
+        public List<Package> Packages { get; set; }
     }
 
     public class Package
     {
-        public string NslId { get; set; }
-        public string SequenceNumber { get; set; }
+        [XmlElement("NSLID")]
+        public string NSLID { get; set; }
+        [XmlElement("Sequence")]
+        public string Sequence { get; set; }
+        [XmlElement("LogicalPage")]
         public string LogicalPage { get; set; }
+        [XmlElement("DivertCode")]
         public string DivertCode { get; set; }
+        [XmlElement("EnclosureCode")]
         public string EnclosureCode { get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,9 +4,14 @@ using System.Linq;
 using System.Text;
 using log4net;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
+using System.Collections;
+using System.Xml;
 
 namespace package_loader
 {
+
     class Program
     {
         private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
@@ -24,7 +29,15 @@ namespace package_loader
                 if (extension.ToLower() == "xml")
                 {
                     log.Info("Begin processing on " + args[0]);
-                    ReadXml(xmlfile);
+                    Job Job = DeserializeXMLFileToObject<Job>(xmlfile);
+                    for (var i = 0; i < Job.Packages.Count; i++)
+                    {
+                        Console.WriteLine("Sequence: " + Job.Packages[i].Sequence);
+                        Console.WriteLine("LogPage: " + Job.Packages[i].LogicalPage);
+                        Console.WriteLine();
+                    }
+
+                       // Console.WriteLine(Job.Packages[3].Sequence);
                 }
                 else
                 {
@@ -32,12 +45,27 @@ namespace package_loader
                 }
                 Console.ReadKey();
             }
-
         }
 
-        static void ReadXml(string xml)
+        public static Job DeserializeXMLFileToObject<Job>(string path)
         {
+            Job returnObject = default(Job);
+            if (string.IsNullOrEmpty(path)) return default(Job);
 
+            try
+            {
+                StreamReader xmlStream = new StreamReader(path);
+                XmlSerializer serializer = new XmlSerializer(typeof(Job));
+                returnObject = (Job)serializer.Deserialize(xmlStream);
+            }
+            catch (Exception ex)
+            {
+                log.Error("XML Deserialization failed", ex);
+            }
+            return returnObject;
         }
+
+
+
     }
 }
