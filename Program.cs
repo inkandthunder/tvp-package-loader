0a865fb5c29fea600d6e1618c5dc84baa4a96117
Scott DeHaan <scottdehaan@gmail.com>
2018-02-19 08:08:55 -0600

    Added argument definitions

--- Program.cs
+++ Program.cs
@@ -2,14 +2,44 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using log4net;
 using System.Threading.Tasks;
 
 namespace package_loader
 {
     class Program
     {
+        private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         static void Main(string[] args)
         {
+            if (args.Length == 0)
+            {
+                log.Info("No arguments passed. Exiting Application.");
+            }
+            else
+            {
+                string xmlfile = args[0];	
+                string extension = xmlfile.Substring(xmlfile.LastIndexOf('.') + 1);
+                if (extension.ToLower() == "xml")
+                {
+                    log.Info("Begin processing on " + args[0]);
+                    Console.WriteLine("Valid Xml File");
+                }
+                else
+                {
+                    log.Debug("Invalid filetype. Please pass an XML file");
+                    Console.WriteLine("Invalid file,Please Pass xml file");
+                }
+
+                Console.ReadKey();
+            }
+
         }
+
+        //static void ReadXml()
+        //{
+
+        //}
     }
 }
